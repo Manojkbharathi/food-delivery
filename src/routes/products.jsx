59a236c20611acbe54dcae9312750d2246cc4349
Manojkbharathi <manojbharathi00@gmail.com>
2023-08-05 21:28:10 +0530

    added data

--- src/routes/products.jsx
+++ src/routes/products.jsx
@@ -3,18 +3,65 @@ import {
   AiOutlineFacebook,
   AiOutlineTwitter,
 } from 'react-icons/ai';
-const handleLogout = async () => {
-  signOut(auth)
-    .then(() => {
-      setUserName('Signed out successfully');
-      navigate('/logIn');
-    })
-    .catch((err) => console.log(err.message));
-};
+import React, { useState } from 'react';
+import '../components/footer.css';
+import data from '../components/data.json';
+import '../components/data.json';
+import '../components/products.css';
+
 const Products = () => {
+  const [selected, setSelected] = useState(0);
+  const [selectedDish, setSelectedDish] = useState('pizza');
+  console.log(data[selectedDish]);
+  const handleClick = (item, key) => {
+    setSelected(key);
+    setSelectedDish(item);
+  };
+  let product = Object.getOwnPropertyNames(data);
+  const [users, setUsers] = useState({ name: '', email: '' });
+  const { name, email } = users;
+  function handleOnchange(event) {
+    const value = event.target.value;
+    const key = event.target.name;
+    setUsers({ ...users, [key]: value });
+  }
+
   return (
-    <div>
-      Products
+    <div className='wrapper'>
+      <div className='title'>
+        <h2>Menu</h2>
+        <div className='menu-list-container'>
+          <div className='menu-list'>
+            {product.map((item, key) => {
+              return (
+                <div
+                  key={key}
+                  onClick={() => handleClick(item, key)}
+                  className={
+                    selected === key
+                      ? 'item-container active'
+                      : 'item-container'
+                  }
+                >
+                  <p>{item}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className='food'>
+          <div className='pizza'>
+            {data[selectedDish].map((dish) => (
+              <div className='pizza-img-container' key={dish.id}>
+                {console.log(dish)}
+                <img className='pizza-img' src={dish.image} alt='' />
+                <h3 className='dish-name'>{dish.name}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
       <footer className='footer'>
         <form
           className='form'
